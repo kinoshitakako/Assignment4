32984edfb93dbc66ef3475fbd7698a4af165aa79
KakoKinoshita <140261493+kinoshitakako@users.noreply.github.com>
2023-09-05 07:52:29 +0000

    [Finish]課題4

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,66 @@
+class BooksController < ApplicationController
+
+
+  def show
+    @books = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_comment = BookComment.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully"
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    @user = current_user
+    render :index
+    end
+  end
+
+  def edit
+    book = Book.find(params[:id])
+    if book.user == current_user
+       @book = Book.find(params[:id])
+    else
+      redirect_to books_path
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def destroy
+    book= Book.find(params[:id])
+    if book.user == current_user
+    book.destroy
+    end
+    redirect_to books_path
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.user == current_user
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+            redirect_to book_path(@book.id), notice: "You have updated book successfully"
+        else
+            render :edit
+        end
+    end
+  end
+
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :new, :update, :index]
+  
+  get 'home/about' => 'homes#about', as: 'about'
+end
